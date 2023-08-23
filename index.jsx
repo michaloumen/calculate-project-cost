e4f6c1de03d7aa4c21d62c3626eb70f437220b9c
michaloumen <michelle.mendonca@minu.co>
2023-08-23 00:49:46 -0300

    funcionando

--- index.jsx
+++ index.jsx
@@ -1,44 +1,53 @@
-function calculateProjectCost(monthlySalary, startDate, endDate) {
+function calculateProjectCost(monthlySalary, projects) {
   function calculateDailyCost(salary, daysInMonth) {
     return salary / daysInMonth;
   }
 
-  let totalCost = 0;
+  let totalCosts = {};
 
-  for (let year = startDate.getFullYear(); year <= endDate.getFullYear(); year++) {
-    const startMonth = (year === startDate.getFullYear()) ? startDate.getMonth() : 0;
-    const endMonth = (year === endDate.getFullYear()) ? endDate.getMonth() : 11;
+  for (const project of projects) {
+    const { startDate, endDate } = project;
 
-    for (let month = startMonth; month <= endMonth; month++) {
+    // Calcula o custo total do projeto
+    let projectTotalCost = 0;
+
+    for (let date = startDate.getTime(); date <= endDate.getTime(); date += 24 * 60 * 60 * 1000) {
+      const currentDate = new Date(date);
+      const year = currentDate.getFullYear();
+      const month = currentDate.getMonth();
       const daysInMonth = new Date(year, month + 1, 0).getDate();
       const dailyCost = calculateDailyCost(monthlySalary, daysInMonth);
 
-      if (year === startDate.getFullYear() && year === endDate.getFullYear() && month === startDate.getMonth() && month === endDate.getMonth()) {
-        const daysInProject = (endDate.getDate() - startDate.getDate()) + 1;
-        totalCost += daysInProject * dailyCost;
-      } else if (year === startDate.getFullYear() && month === startDate.getMonth()) {
-        const daysInStartMonth = (new Date(year, month + 1, 0).getDate() - startDate.getDate()) + 1;
-        totalCost += daysInStartMonth * dailyCost;
-      } else if (year === endDate.getFullYear() && month === endDate.getMonth()) {
-        const daysInEndMonth = endDate.getDate();
-        totalCost += daysInEndMonth * dailyCost;
-      } else {
-        totalCost += daysInMonth * dailyCost;
-      }
+      // Verifica os projetos que ocorrem no mesmo dia
+      const sameDayProjects = projects.filter(otherProject =>
+        otherProject !== project &&
+        otherProject.startDate <= currentDate &&
+        otherProject.endDate >= currentDate
+      );
+
+      // Divide o dailyCost igualmente entre os projetos que acontecem no mesmo dia
+      const dividedDailyCost = sameDayProjects.length > 0 ? dailyCost / (sameDayProjects.length + 1) : dailyCost;
+
+      projectTotalCost += dividedDailyCost;
     }
+
+    totalCosts[project.name] = projectTotalCost;
   }
 
-  return totalCost;
+  return totalCosts;
 }
 
-// Defina o salário mensal e as datas de início e fim do projeto
-const monthlySalary = 3000; // Salário mensal do time em reais
-const startDate = new Date(2021, 1, 28); // 27 de fevereiro de 2023
-const endDate = new Date(2021, 2, 1);    // 1 de março de 2023
+// Defina o salário mensal e os projetos
+const monthlySalary = 3100; // Salário mensal do time em reais
+const projects = [
+  { name: 'Projeto A', startDate: new Date(2023, 3, 01), endDate: new Date(2023, 3, 02) },
+  // { name: 'Projeto B', startDate: new Date(2023, 3, 01), endDate: new Date(2023, 3, 01) },
+  // { name: 'Projeto C', startDate: new Date(2023, 7, 01), endDate: new Date(2023, 7, 31) },
+  // Adicione mais projetos aqui se necessário
+];
 
-// Calcule o custo do projeto
-const projectCost = calculateProjectCost(monthlySalary, startDate, endDate);
-
-console.log(`O projeto custou ${projectCost.toFixed(2)} reais.`);
-
-
+// Calcule o custo de cada projeto individualmente
+const projectCosts = calculateProjectCost(monthlySalary, projects);
+for (const projectName in projectCosts) {
+  console.log(`O custo do projeto ${projectName} foi de ${projectCosts[projectName].toFixed(2)} reais.`);
+}
