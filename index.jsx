2389358f76300c2d667937b1a790313c1eb8e4ff
michaloumen <michelle.mendonca@minu.co>
2023-08-23 01:03:26 -0300

    adiciona meses de 1 a 12

--- index.jsx
+++ index.jsx
@@ -1,3 +1,7 @@
+function createDate(year, month, day) {
+  return new Date(year, month - 1, day);
+}
+
 function calculateProjectCost(monthlySalary, projects) {
   function calculateDailyCost(salary, daysInMonth) {
     return salary / daysInMonth;
@@ -14,8 +18,8 @@ function calculateProjectCost(monthlySalary, projects) {
     for (let date = startDate.getTime(); date <= endDate.getTime(); date += 24 * 60 * 60 * 1000) {
       const currentDate = new Date(date);
       const year = currentDate.getFullYear();
-      const month = currentDate.getMonth();
-      const daysInMonth = new Date(year, month + 1, 0).getDate();
+      const month = currentDate.getMonth() + 1; // Ajusta os meses para 1-12
+      const daysInMonth = new Date(year, month, 0).getDate();
       const dailyCost = calculateDailyCost(monthlySalary, daysInMonth);
 
       // Verifica os projetos que ocorrem no mesmo dia
@@ -40,9 +44,9 @@ function calculateProjectCost(monthlySalary, projects) {
 // Defina o salário mensal e os projetos
 const monthlySalary = 3100; // Salário mensal do time em reais
 const projects = [
-  { name: 'Projeto A', startDate: new Date(2023, 3, 01), endDate: new Date(2023, 3, 02) },
-  // { name: 'Projeto B', startDate: new Date(2023, 3, 01), endDate: new Date(2023, 3, 01) },
-  // { name: 'Projeto C', startDate: new Date(2023, 7, 01), endDate: new Date(2023, 7, 31) },
+  { name: 'Projeto A', startDate: createDate(2023, 2, 27), endDate: createDate(2023, 3, 02) },
+  { name: 'Projeto B', startDate: createDate(2023, 3, 02), endDate: createDate(2023, 3, 03) },
+  // { name: 'Projeto C', startDate: createDate(2023, 7, 01), endDate: createDate(2023, 7, 31) },
   // Adicione mais projetos aqui se necessário
 ];
 
@@ -51,3 +55,7 @@ const projectCosts = calculateProjectCost(monthlySalary, projects);
 for (const projectName in projectCosts) {
   console.log(`O custo do projeto ${projectName} foi de ${projectCosts[projectName].toFixed(2)} reais.`);
 }
+
+// considere que o se o projeto teve 1 dia ele começou e terminou no mesmo dia 
+// se ele teve 2 dias, ele começou em um dia e terminou no próximo dia consecutivo
+// ou seja, o endDate deve ser o último dia que o projeto ainda estava sendo executado
